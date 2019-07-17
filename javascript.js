863f7f2605bf293871bdf861cb32b2d026f9c54d
Programador Novato <info@programadornovato.com>
2019-07-17 03:14:01 -0500

    03.- Jquery🌇  de 0 a 100 [Manipulacion de Selectores]
    Video:
    https://youtu.be/ysLhsx7Z6CY

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-    //alert("Hola mundo desde jquery");
-    //let varBoton=$('button').html();
-    //console.log(varBoton);
-    //$('button').html("Cambiado desde jquery");
-    //$('.btn').html("Cambiado desde la clase");
-    //$('#boton').html("Cambiado desde el id");
-    //$('#subcontenedor button').html("boton desde subcontenedor");
-    $('button:first').html("primero");
-    $('button:last').html("ultimo");
+    $('#boton').addClass('btn-danger');
+    $('#boton').removeClass('btn-primary');
+
+    $('#subcontenedor').append('<button type="button" class="btn btn-danger">agregado con append</button>');
+    $('#subcontenedor').prepend('<button type="button" class="btn btn-danger">agregado con prepend</button>');
+
+    $('#boton').css({'color':'blue','background-color':'yellow'});
+    //$('#boton').css('background-color', 'yellow');
+
+    //$('#subcontenedor').remove();
+    //$('#subcontenedor').hide();
+    $('img').attr({'src':'logo2.png','width':'400px'});
+    //$('img').attr('width', '300px');
 });
 
-//document.querySelector('#boton').innerHTML="Cambiado  desde js";
