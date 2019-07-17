2feea70b7a267358374ae65c2d130b0ef18b6324
Programador Novato <info@programadornovato.com>
2019-07-17 00:48:58 -0500

    02.- Jquery🌇  de 0 a 100 [Selectores]
    https://youtu.be/Qvtqvya4eEc

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    alert("Hola mundo desde jquery");
+    //alert("Hola mundo desde jquery");
+    //let varBoton=$('button').html();
+    //console.log(varBoton);
+    //$('button').html("Cambiado desde jquery");
+    //$('.btn').html("Cambiado desde la clase");
+    //$('#boton').html("Cambiado desde el id");
+    //$('#subcontenedor button').html("boton desde subcontenedor");
+    $('button:first').html("primero");
+    $('button:last').html("ultimo");
 });
+
+//document.querySelector('#boton').innerHTML="Cambiado  desde js";
