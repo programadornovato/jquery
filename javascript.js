f560424fc3e5396bb38e27d2ef3869c2ec1b8a87
Programador Novato <info@programadornovato.com>
2019-07-17 23:50:01 -0500

    06.- Jquery🌇  de 0 a 100 [Efectos]
    Video:
    https://youtu.be/zWpOe8PKZq8

--- javascript.js
+++ javascript.js
@@ -19,6 +19,38 @@ $(document).ready(function(){
         e.preventDefault();
         $('img').toggleClass('d-none');
     });
+    $('#subcontenedor button:nth-child(4)').click(function (e) { 
+        e.preventDefault();
+        $('img').hide('slow');
+    });
+    $('#subcontenedor button:nth-child(5)').click(function (e) { 
+        e.preventDefault();
+        $('img').show('fast');
+    });
+    $('#subcontenedor button:nth-child(6)').click(function (e) { 
+        e.preventDefault();
+        $('img').toggle(300);
+    });
+    $('#subcontenedor button:nth-child(7)').click(function (e) { 
+        e.preventDefault();
+        $('img').fadeIn(300);
+    });
+    $('#subcontenedor button:nth-child(8)').click(function (e) { 
+        e.preventDefault();
+        $('img').fadeOut(300);
+    });
+    $('#subcontenedor button:nth-child(9)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideDown(300);
+    });
+    $('#subcontenedor button:nth-child(10)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideUp(300);
+    });
+    $('#subcontenedor button:nth-child(11)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideToggle(300);
+    });
     let textoUI=$('#texto');
     textoUI.keyup(function (e) { 
         e.preventDefault();
