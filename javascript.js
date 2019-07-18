8ad7d6963dccb7a725843224cba4d857c8ddd041
Programador Novato <info@programadornovato.com>
2019-07-17 19:47:19 -0500

    05.- Jquery🌇  de 0 a 100 [Evento keyup y submit]
    Video:
    https://youtu.be/vICYW7t5S58

--- javascript.js
+++ javascript.js
@@ -19,6 +19,14 @@ $(document).ready(function(){
         e.preventDefault();
         $('img').toggleClass('d-none');
     });
-
+    let textoUI=$('#texto');
+    textoUI.keyup(function (e) { 
+        e.preventDefault();
+        $('#saludo').html("Hola "+textoUI.val());
+    });
+    $('form').submit(function (e) { 
+        e.preventDefault();
+        
+    });
 });
 
