ac57c16a4f5730e66fe0c68abac96c9d4e74ced9
Programador Novato <info@programadornovato.com>
2019-07-23 12:04:37 -0500

    08.- Jquery🌇  de 0 a 100 [Pluging Chosen]
    Video:
    https://youtu.be/e-WpE3slYvU

--- javascript.js
+++ javascript.js
@@ -1,89 +1,17 @@
 $(document).ready(function(){
-    $('#boton').click(function (e) { 
-        e.preventDefault();
-        $('img').attr('src', 'logo2.png');
-    });
-    $('#subcontenedor button:nth-child(1)').click(function (e) { 
-        e.preventDefault();
-        $('img').attr('width', '200px');
-    });
-    $('#subcontenedor button:nth-child(2)').click(function (e) { 
-        e.preventDefault();
-        let margen= $('img').css('margin-left');
-        //alert(margen);
-        margen=parseInt( margen.replace('px',''));
-        margen=margen+30;
-        $('img').css('margin-left', margen+'px');
-    });
-    $('#subcontenedor button:nth-child(3)').click(function (e) { 
-        e.preventDefault();
-        $('img').toggleClass('d-none');
-    });
-    $('#subcontenedor button:nth-child(4)').click(function (e) { 
-        e.preventDefault();
-        $('img').hide('slow');
-    });
-    $('#subcontenedor button:nth-child(5)').click(function (e) { 
-        e.preventDefault();
-        $('img').show('fast');
-    });
-    $('#subcontenedor button:nth-child(6)').click(function (e) { 
-        e.preventDefault();
-        $('img').toggle(300);
-    });
-    $('#subcontenedor button:nth-child(7)').click(function (e) { 
-        e.preventDefault();
-        $('img').fadeIn(300);
-    });
-    $('#subcontenedor button:nth-child(8)').click(function (e) { 
-        e.preventDefault();
-        $('img').fadeOut(300);
-    });
-    $('#subcontenedor button:nth-child(9)').click(function (e) { 
-        e.preventDefault();
-        $('img').slideDown(300);
-    });
-    $('#subcontenedor button:nth-child(10)').click(function (e) { 
-        e.preventDefault();
-        $('img').slideUp(300);
-    });
-    $('#subcontenedor button:nth-child(11)').click(function (e) { 
-        e.preventDefault();
-        $('img').slideToggle(300);
-    });
-    $('#subcontenedor button:nth-child(12)').click(function (e) { 
-        e.preventDefault();
-        $('img').animate({
-            'margin-left':'+=100px',
-            'opacity':'0.5',
-            'width':'150px'
-        },1000,
-        function(){
-            $('img').animate({
-                'margin-left':'-=100px',
-                'opacity':'1',
-                'width':'200px'
-            },2000);
-        }
-        );
-    });
-    $('#subir').click(function (e) { 
-        e.preventDefault();
-        $('html, body').animate({scrollTop:0},1000);
-    });
-    $('#bajar').click(function (e) { 
-        e.preventDefault();
-        $('html, body').animate({scrollTop:$(document).height()-$(window).height()},1000);
-    });
+    //$('.chosen-select').chosen();
 
-    let textoUI=$('#texto');
-    textoUI.keyup(function (e) { 
-        e.preventDefault();
-        $('#saludo').html("Hola "+textoUI.val());
-    });
-    $('form').submit(function (e) { 
-        e.preventDefault();
-        
+    //$('.chosen-select').chosen({no_results_text:"Ups no hay nada que diga: "});
+
+    //$('.chosen-select').chosen({disable_search_threshold:11});
+
+    //$('.chosen-select').chosen({max_selected_options:3});
+
+    //$('.chosen-select').chosen({rtl:true,max_selected_options:3});
+
+    $('.chosen-select').chosen().change(function(){
+        //alert("cambio");
+        alert($(this).val());
     });
 });
 
