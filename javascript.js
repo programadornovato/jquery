4bfec39c9c1fc5d9cf8c6bd506f609bfa7fda93b
Programador Novato <info@programadornovato.com>
2019-07-24 11:51:56 -0500

    10.- Jquery🌇  de 0 a 100 [pluging Carousel/slide]
    Video:
    https://youtu.be/SBCFOHy3bB0

--- javascript.js
+++ javascript.js
@@ -1,27 +1,16 @@
 $(document).ready(function(){
-    $('#fecha').daterangepicker({
-        locale:{
-            format:"YYYY-MM-DD hh:mm:ss"
-        },
-        timePicker:true,
-        startDate:moment().startOf('hour'),
-        endDate:moment().startOf('hour').add(48,'hour')
-    },function(inicio,fin){
-        console.log(inicio.format('YYYY-MM-DD hh:mm:ss')+" - "+fin.format('YYYY-MM-DD hh:mm:ss'));
-    });
-    $('#fecha2').daterangepicker({
-        locale:{
-            format:"YYYY-MM-DD hh:mm:ss"
-        },
-        singleDatePicker:true,
-        showDropdowns:true,
-        timePicker:true,
-        minYear:2000,
-        maxYear:moment().format('YYYY'),
-        startDate:moment().startOf('hour'),
-        endDate:moment().startOf('hour').add(48,'hour')
-    },function(inicio,fin){
-        console.log(inicio.format('YYYY-MM-DD hh:mm:ss')+" - "+fin.format('YYYY-MM-DD hh:mm:ss'));
+    //$('.slider').bxSlider();
+    $('.bxslider').bxSlider({
+        auto:true,
+        speed:500,
+        autoControls:true,
+        stopAutoOnClick:true,
+        pager:true,
+        pagerType:'short',
+        //slideWidth:100,
+        mode:'vertical',
+        randomStart:true,
+        infiniteLoop:false
     });
 });
 
