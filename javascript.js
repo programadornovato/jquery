9e78f63c273940a2f5c7aba0cfe7ec95364d82b6
Programador Novato <info@programadornovato.com>
2019-07-24 08:23:16 -0500

    09.- Jquery🌇  de 0 a 100 [Pluging DateRangePicker/DateTimePicker ]
    Video:
    https://youtu.be/AaQ6xuNtvo0

--- javascript.js
+++ javascript.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
-    //$('.chosen-select').chosen();
-
-    //$('.chosen-select').chosen({no_results_text:"Ups no hay nada que diga: "});
-
-    //$('.chosen-select').chosen({disable_search_threshold:11});
-
-    //$('.chosen-select').chosen({max_selected_options:3});
-
-    //$('.chosen-select').chosen({rtl:true,max_selected_options:3});
-
-    $('.chosen-select').chosen().change(function(){
-        //alert("cambio");
-        alert($(this).val());
+    $('#fecha').daterangepicker({
+        locale:{
+            format:"YYYY-MM-DD hh:mm:ss"
+        },
+        timePicker:true,
+        startDate:moment().startOf('hour'),
+        endDate:moment().startOf('hour').add(48,'hour')
+    },function(inicio,fin){
+        console.log(inicio.format('YYYY-MM-DD hh:mm:ss')+" - "+fin.format('YYYY-MM-DD hh:mm:ss'));
+    });
+    $('#fecha2').daterangepicker({
+        locale:{
+            format:"YYYY-MM-DD hh:mm:ss"
+        },
+        singleDatePicker:true,
+        showDropdowns:true,
+        timePicker:true,
+        minYear:2000,
+        maxYear:moment().format('YYYY'),
+        startDate:moment().startOf('hour'),
+        endDate:moment().startOf('hour').add(48,'hour')
+    },function(inicio,fin){
+        console.log(inicio.format('YYYY-MM-DD hh:mm:ss')+" - "+fin.format('YYYY-MM-DD hh:mm:ss'));
     });
 });
 
